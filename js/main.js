8c5f1c58a49dd2b55acc380119ab716eae007632
ralph6711 <64796510+rahulajadhav@users.noreply.github.com>
2022-07-10 17:43:00 -0400

    agefunction added

--- js/main.js
+++ js/main.js
@@ -676,4 +676,17 @@ $(function() {
 
   })
 
+
+  //age update
+  var dob = new Date("01/11/1994");
+  //calculate month difference from current date in time
+  var month_diff = Date.now() - dob.getTime();
+  //convert the calculated difference in date format
+  var age_dt = new Date(month_diff);
+  //extract year from date
+  var year = age_dt.getUTCFullYear();
+  //now calculate the age of the user
+  var age = Math.abs(year - 1970);
+  //display the calculated age
+  document.getElementById("age").innerHTML = age;
 });
